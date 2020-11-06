f090cdb80b8071624ebcbb02b5bd1b665f2f2bd2
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-11-06 14:17:25 +0600

    first

---
+++ ToDoList/ItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  ItemModel.swift
+//  ToDoList
+//
+//  Created by Kairat Zukhra on 06.11.2020.
+//
+
+import UIKit
+
+struct Item {
+    var nameItem: String?
+    var dateItem: Date?
+}
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var listOfItems = [Item]()
+    var currentItem: Item?
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listOfItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mainList", for: indexPath) as! CustomTableViewCell
+        
+        let item = listOfItems[indexPath.row]
+        
+        cell.newItemLabel.text = item.nameItem
+        cell.daysLeftLabel.text = item.dateItem!.daysFromNowToString()
+
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        
+        guard let newItemVC = segue.source as? NewItemTableViewController else { return }
+        
+        newItemVC.itemNameTextField.text = currentItem?.nameItem
+        newItemVC.datePicker.date = (currentItem?.dateItem)!
+        
+        listOfItems.append(currentItem!)
+        tableView.reloadData()
+    }
 }
 
