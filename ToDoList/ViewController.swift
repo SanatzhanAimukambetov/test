8b2875403ea96550b82ffe05faf063b70b04f8d3
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-11-09 20:24:13 +0600

    SnapKit was added

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -6,17 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var listOfItems = [Item]()
-    var currentItem: Item?
     
     @IBOutlet var tableView: UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listOfItems.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -29,21 +37,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
         return cell
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
     
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
         guard let newItemVC = segue.source as? NewItemTableViewController else { return }
         
-        newItemVC.itemNameTextField.text = currentItem?.nameItem
-        newItemVC.datePicker.date = (currentItem?.dateItem)!
+        let currentItem = Item(nameItem: newItemVC.itemNameTextField.text, dateItem: newItemVC.datePicker.date)
         
-        listOfItems.append(currentItem!)
+        listOfItems.append(currentItem)
         tableView.reloadData()
+    }
+    
+    // MARK: Adding new item (view appearing)
+    @IBAction func addNewItem(_ sender: UIBarButtonItem) {
+        
+        let addNewItemView = addItemView()
+        
+        self.view.addSubview(addNewItemView)
+    }
+    
+    
+    // MARK: Deleting item
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        _ = listOfItems[indexPath.row]
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (_, _, _) in self.listOfItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
 }
 
---
+++ ToDoList/addItemView.swift
@@ -0,0 +1,33 @@
+//
+//  addItemView.swift
+//  ToDoList
+//
+//  Created by Kairat Zukhra on 09.11.2020.
+//
+
+import UIKit
+
+class addItemView: UIView {
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add", for: .highlighted)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+    
+}
